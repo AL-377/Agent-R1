6b0a3611c5d5874924eadb601ec6236426f0ce1b
juntinglu <juntinglu@bytedance.com>
2026-01-17 17:10:43 +0800

    update

--- scripts/data_carecall_process.sh
+++ scripts/data_carecall_process.sh
@@ -1,7 +1,7 @@
 python3 examples/dataset/carecall/memory_preprocess.py \
     --input examples/dataset/carecall/carecall-memory_en_auto_translated.json \
     --output examples/dataset/carecall/cmemory_eval_dataset_all.json \
-    --max-patients 800 \
+    --max-patients 900 \
     --cache-dir /opt/tiger/Agent-R1/examples/dataset/carecall-cache \
     --model DeepSeek-R1 \
     --workers 8
