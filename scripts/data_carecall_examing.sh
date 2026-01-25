79fc177023c4dc005086ee168e345a1016ea8cf9
juntinglu <juntinglu@bytedance.com>
2026-01-25 19:34:59 +0800

    update

---
+++ scripts/data_carecall_examing.sh
@@ -0,0 +1,3 @@
+python examples/dataset/carecall/examiner_agent_preprocess.py \
+  --input_file examples/dataset/carecall-cache/patient_0.json \
+  --output_file examples/dataset/carecall/patient_0_examiner.json
