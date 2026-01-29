7b35504adffddcf20ad733bd03365df49750e0d8
juntinglu <juntinglu@bytedance.com>
2026-01-29 11:58:44 +0800

    add: annotate

--- agent_r1/src/reward_score/__init__.py
+++ agent_r1/src/reward_score/__init__.py
@@ -63,6 +63,7 @@ def _default_compute_score_format_answer(data_source, solution_str, ground_truth
         return float(res[0])
     
 def _default_compute_score(data_source, solution_str, ground_truth, extra_info=None):
+    # 最后reward tensor 赋值来自score
     return {
         "score": _default_compute_score_format_answer(data_source, solution_str, ground_truth, extra_info),
         "acc": _default_compute_score_answer(data_source, solution_str, ground_truth, extra_info),
